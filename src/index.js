706961ff555773de1579bccacc143fb6e82b9b2b
Cyan <917244295@qq.com>
2023-02-15 09:29:29 +0800

    add tab-scroll component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import lookEmpty from './components/look-empty.vue'
+import tabScroll from './components/tab-scroll.vue'
 export default {
   install(Vue) {
     Vue.component('look-empty', lookEmpty)
+    Vue.component('look-tab-scroll', tabScroll)
   }
 }
+
+
