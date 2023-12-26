f3e829fb7492ff1e66a7fb8885f0ec8e75a49713
chenshiyan <917244295@qq.com>
2023-12-26 10:20:50 +0800

    refactor: 查重接口调整

--- changelog.js
+++ changelog.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
-const md = require('markdown-it')();
 
 const changelog = fs.readFileSync('CHANGELOG.md', 'utf-8');
-const changelogHTML = md.render(changelog);
-
 const readme = fs.readFileSync('README.md', 'utf-8');
 
-const updatedReadme = readme + changelogHTML;
+const updatedReadme = readme + changelog;
 
 fs.writeFileSync('README.md', updatedReadme);
--- utils/api.js
+++ utils/api.js
@@ -5,11 +5,6 @@ export const searchRepeated = function (url, data) {
   return request({
     url: `${url}/searchRepeated/batchSearch`,
     method: 'post',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    },
-    data: Qs.stringify(data),
+    data
   });
 };
-
-// http://192.168.10.28:7076
