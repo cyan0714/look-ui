44cb711abdcac89bfa414f7bc1bea62c9eeef71a
chenshiyan <917244295@qq.com>
2024-01-18 10:35:24 +0800

    style: 关联公文调整

--- packages/look-associated-document/src/main.vue
+++ packages/look-associated-document/src/main.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="look-associated-document">
     <div class="left-container">
-      <div class="search-area">
-        <span class="txt">关键字:</span>
-        <el-input
-          class="lookui-input"
-          v-model="keywords"
-          placeholder="可输入关键字进行匹配"
-          clearable></el-input>
-        <el-button class="lookui-btn" type="primary" @click="handleQuery">查询</el-button>
+      <div class="header">
+        <div class="range-area">
+          <span class="txt">范围:</span>
+          <div class="btn-item-wrap">
+            <div
+              v-for="(item, index) in range"
+              :class="['btn-item', currentIndex === index ? 'btn-item-actived' : '']"
+              :key="index"
+              @click="handleClick(index)">
+              {{ item.name }}
+            </div>
+          </div>
+        </div>
+        <div class="search-area">
+          <el-input
+            class="lookui-input"
+            v-model="keywords"
+            placeholder="请输入公文标题或来文单位"
+            clearable></el-input>
+          <el-button class="lookui-btn" type="primary" @click="handleQuery">查询</el-button>
+        </div>
       </div>
       <div class="list-area">
         <div class="la-item" v-for="(item, index) in list" :key="index">
@@ -68,6 +81,21 @@ export default {
   components: {},
   data() {
     return {
+      currentIndex: 0,
+      range: [
+        {
+          name: '个人',
+          id: 1,
+        },
+        {
+          name: '部门',
+          id: 2,
+        },
+        {
+          name: '单位',
+          id: 3,
+        },
+      ],
       keywords: '',
       selectedList: [
         {
@@ -115,6 +143,9 @@ export default {
   created() {},
   mounted() {},
   methods: {
+    handleClick(index) {
+      this.currentIndex = index;
+    },
     handleQuery() {
       // if (this.keywords.trim() === '') {
       //   this.toggleTag(this.currentMissionType)
@@ -138,27 +169,57 @@ export default {
     height: 100%;
     background-color: #fff;
     padding: 12px;
-    .search-area {
+    .header {
       display: flex;
-      align-items: center;
-      flex-wrap: nowrap;
-      span.txt {
-        font-weight: normal;
-        font-size: 17px;
-        margin: 0 8px;
-      }
-      .lookui-input {
-        width: 250px;
-        ::v-deep .el-input__inner {
-          height: 34px;
+      justify-content: space-between;
+      .range-area {
+        display: flex;
+        align-items: center;
+        .btn-item-wrap {
+          display: flex;
+          align-items: center;
+          flex-wrap: nowrap;
+          border: 1px solid #d9d9d9;
+          border-left: none;
+          margin-left: 10px;
+          border-radius: 4px;
+          .btn-item {
+            padding: 4px 16px 6px;
+            cursor: pointer;
+            border-left: 1px solid #d9d9d9;
+            &:first-child {
+              border-top-left-radius: 4px;
+              border-bottom-left-radius: 4px;
+            }
+            &:last-child {
+              border-top-right-radius: 4px;
+              border-bottom-right-radius: 4px;
+            }
+            &.btn-item-actived {
+              background-color: #506eda;
+              color: #fff;
+            }
+          }
         }
-        ::v-deep .el-input__icon {
-          line-height: 34px;
-        }
       }
-      .lookui-btn {
-        padding: 9px 18px;
-        border-radius: 0;
+
+      .search-area {
+        display: flex;
+        align-items: center;
+        flex-wrap: nowrap;
+        .lookui-input {
+          width: 250px;
+          ::v-deep .el-input__inner {
+            height: 34px;
+          }
+          ::v-deep .el-input__icon {
+            line-height: 34px;
+          }
+        }
+        .lookui-btn {
+          padding: 9px 18px;
+          border-radius: 0;
+        }
       }
     }
     .list-area {
